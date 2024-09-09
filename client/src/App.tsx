96cc399a2ddbe672acf80b12d293cef5930b7cf9
MohitCode17 <mohit.codes17@gmail.com>
2024-09-09 17:34:45 +0530

    initialize-frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <>
+    <h1 className="text-blue-400">Let's Create Food App</h1>
+    <Button className="bg-orange hover:bg-hoverOrange">Click Here</Button>
+    </>
+  )
+}
+
+export default App
