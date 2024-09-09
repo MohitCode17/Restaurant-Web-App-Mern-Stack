610a8d8045474b7304a8e7df1b772f17554277ef
MohitCode17 <mohit.codes17@gmail.com>
2024-09-09 21:55:10 +0530

    add react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,29 @@
-import Signup from "./pages/auth/Signup"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/Signup";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+]);
 
 const App = () => {
   return (
-    <>
-    <Signup/>
-    </>
-  )
-}
+    <main>
+      <RouterProvider router={appRouter}></RouterProvider>
+    </main>
+  );
+};
 
-export default App
+export default App;
