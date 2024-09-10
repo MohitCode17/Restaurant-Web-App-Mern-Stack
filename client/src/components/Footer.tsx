775584f70eb49f03e47abc00d267a01af4b87aca
MohitCode17 <mohit.codes17@gmail.com>
2024-09-10 22:35:40 +0530

    add footer ui

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="text-center text-gray-800 py-8 px-4 border-t-[1px]">
+      <p>&copy; 2024 Restaurant. All Right Reserved</p>
+    </div>
+  );
 };
 
 export default Footer;
