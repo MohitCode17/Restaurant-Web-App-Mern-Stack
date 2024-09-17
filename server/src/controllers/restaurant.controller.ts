087590b30154089bd5faa178563d02c61f011aa4
MohitCode17 <mohit.codes17@gmail.com>
2024-09-17 21:50:21 +0530

    add get & update restaurant controller & route

--- server/src/controllers/restaurant.controller.ts
+++ server/src/controllers/restaurant.controller.ts
@@ -49,3 +49,66 @@ export const handleCreateRestaurant = async (req: Request, res: Response) => {
     });
   }
 };
+
+// GET RESTAURANT CONTROLLER
+export const handleGetRestaurant = async (req: Request, res: Response) => {
+  try {
+    const restaurant = await Restaurant.findById({ user: req.id }).populate(
+      "menus"
+    );
+
+    if (!restaurant)
+      return res.status(400).json({
+        success: false,
+        message: "Restaurant not found.",
+      });
+
+    return res.status(200).json({ success: true, restaurant });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+// UPDATE RESTAURANT CONTROLLER
+export const handleUpdateRestaurant = async (req: Request, res: Response) => {
+  try {
+    // GETTING DATA OF RESTAURANT
+    const { restaurantName, city, country, deliveryTime, cuisines } = req.body;
+
+    // const file = req.file; // TO BE IMPLEMENT MULTER CONFIG
+
+    // FIND RESTAURANT
+    const restaurant = await Restaurant.findOne({ user: req.id });
+
+    if (!restaurant)
+      return res.status(400).json({
+        success: false,
+        message: "Restaurant not found.",
+      });
+
+    restaurant.restaurantName = restaurantName;
+    restaurant.city = city;
+    restaurant.country = country;
+    restaurant.deliveryTime = deliveryTime;
+    restaurant.cuisines = JSON.parse(cuisines);
+
+    // if(file) {
+    //     const imageUrl = await uploadImageOnCloudinary();
+    //     restaurant.imageUrl = imageUrl;
+    // }
+
+    await restaurant.save();
+    return res.status(200).json({
+      success: true,
+      message: "Restaurant updated.",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
--- server/src/routes/restaurant.route.ts
+++ server/src/routes/restaurant.route.ts
@@ -1,10 +1,20 @@
 import express from "express";
 import { authenticated } from "../middlewares/authenticated";
-import { handleCreateRestaurant } from "../controllers/restaurant.controller";
+import {
+  handleCreateRestaurant,
+  handleGetRestaurant,
+  handleUpdateRestaurant,
+} from "../controllers/restaurant.controller";
 
 const router = express.Router();
 
 // CREATE RESTAURANT ROUTE
 router.post("/", authenticated, handleCreateRestaurant);
+
+// GET RESTAURANT ROUTE
+router.get("/", authenticated, handleGetRestaurant);
+
+// UPDATE RESTAURANT ROUTE
+router.put("/", authenticated, handleUpdateRestaurant);
 
 export default router;
